f2f34d9ce45766ce70b03a9582c2d4de57587674
Diog011 <diogosantana639@gmail.com>
2026-09-04 00:13:53 +0000

    Corrige infraestrutura e deploy da Function

--- infra/functionapp.tf
+++ infra/functionapp.tf
@@ -18,19 +18,19 @@ resource "azurerm_linux_function_app" "func" {
   name                       = var.function_app_name
   resource_group_name        = azurerm_resource_group.rg.name
   location                   = azurerm_resource_group.rg.location
-  storage_account_name        = azurerm_storage_account.func_storage.name
-  storage_account_access_key  = azurerm_storage_account.func_storage.primary_access_key
-  service_plan_id             = azurerm_service_plan.func_plan.id
+  storage_account_name       = azurerm_storage_account.func_storage.name
+  storage_account_access_key = azurerm_storage_account.func_storage.primary_access_key
+  service_plan_id            = azurerm_service_plan.func_plan.id
 
   site_config {
     application_stack {
       python_version = "3.11"
     }
   }
-  
+
   app_settings = {
     FUNCTIONS_WORKER_RUNTIME = "python"
-  
+
     SQL_SERVER = azurerm_mysql_flexible_server.mysql.fqdn
     SQL_USER   = var.mysql_admin_user
     SQL_PASS   = var.mysql_admin_password
