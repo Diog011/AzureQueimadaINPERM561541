a7f7633572a79deb5ba4123aeedb23dcac8381a2
Diog011 <diogosantana639@gmail.com>
2026-09-04 01:57:54 +0000

    Ativa indexação do worker Python v2

--- infra/functionapp.tf
+++ infra/functionapp.tf
@@ -30,6 +30,7 @@ resource "azurerm_linux_function_app" "func" {
 
   app_settings = {
     FUNCTIONS_WORKER_RUNTIME = "python"
+    AzureWebJobsFeatureFlags = "EnableWorkerIndexing"
 
     SQL_SERVER = azurerm_mysql_flexible_server.mysql.fqdn
     SQL_USER   = var.mysql_admin_user
